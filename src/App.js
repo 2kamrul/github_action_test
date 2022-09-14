845c5f7ba82fbc062a3e2ad0fedc586b22018c38
Kamrul Islam~ <kamrulbeximco@gmail.com>
2022-09-14 15:47:02 +0600

    v7

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Beximco industrial park6
+          Beximco industrial park7
         </p>
         <a
           className="App-link"
